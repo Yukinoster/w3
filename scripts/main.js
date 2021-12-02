62289b43445bb8f07a883326320f627fe0243aa7
Yukinoster <85883093+Yukinoster@users.noreply.github.com>
2021-12-02 11:26:09 +0900

    サンプルページの動作確認変更

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 // Image switcher code
 
-let myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
+let myImage = document.querySelector('img');//基本的にletで変数宣言
+//documentは組み込みのオブジェクト
+//ブラウザが表示しているページ。DOMとも呼ぶ。Document Object Model の略。
+//querySelector はページの一部(HTML要素)を選ぶ関数。
+//
+myImage.onclick = function() {//mayImageをクリックしたら呼び出してねという意味
+  let mySrc = myImage.getAttribute('src');//名前のない関数
   if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
+    myImage.setAttribute ('src','images/firefox2.png');//画像を入れ替えている
   } else {
     myImage.setAttribute ('src','images/firefox-icon.png');
   }
@@ -15,22 +18,25 @@ myImage.onclick = function() {
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-
+//console.log(myHeading);
 function setUserName() {
   let myName = prompt('Please enter your name.');
+  //promptは組み込みの関数。prompt (pop-up) Windowを表示して、ユーザ入力させる。
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    //入力データを特別な場所(変数ではない、ファイルなどに相当)に保存する。
+    myHeading.innerHTML = 'こんにちは, ' + myName;
   }
 }
 
 if(!localStorage.getItem('name')) {
-  setUserName();
+  //以前に特別な場所(ファイルなど)保存してた名前があれば、それを使う。
+  setUserName();//function setUserNameの呼び出し
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'さようなら, ' + storedName;
 }
 
 myButton.onclick = function() {
